18aa6d1381cda7e44166951adb640722d6acdc50
j2ekim <33790074+j2ekim@users.noreply.github.com>
2021-07-28 00:45:47 +0800

    Update zhihu_img_download.py

--- zhihu_img_download.py
+++ zhihu_img_download.py
@@ -12,10 +12,7 @@ import os
 
 
 header = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) '
-                  'AppleWebKit/537.36 (KHTML, '
-                  'like Gecko) Chrome/67.0.3396.99 '
-                  'Safari/537.36',
+    'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0",
     'Host': "www.zhihu.com",
     'Referer': f"https://www.zhihu.com/question/42355466"
 }
